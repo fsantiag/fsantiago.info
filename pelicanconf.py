1aa2935e3c94e4d803c056396a6bf4fd22dc1d4c
Felipe Santiago <fsantiag@thoughtworks.com>
2018-06-01 18:22:06 +0800

    Use separate configuration when deploying to server

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,22 +4,16 @@ from __future__ import unicode_literals
 
 AUTHOR = 'Felipe Santiago'
 SITENAME = 'fsantiago.info'
-SITEURL = 'http://fsantiago.info'
+SITEURL = ''
 SITESUBTITLE= 'Techonology, living experiences and gaming smashed together!'
 
 THEME = 'themes/hyde'
 
 PATH = 'content'
-#STATIC_PATHS = ['2018-03-18']
-#ARTICLE_PATHS = ['2018-03-18']
 
 TIMEZONE = 'Asia/Singapore'
 
 DEFAULT_LANG = 'en'
-
-# Feed generation is usually not desired when developing
-FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
 
 # Social widget
 SOCIAL = (('github', 'https://github.com/fsantiag/'),
