2a148f44407aeecae658951b98646afb1a12a225
Felipe Santiago <jfelipe.sfilho@gmail.com>
2019-10-20 11:11:59 -0300

    Add analytics tracking Id

--- publishconf.py
+++ publishconf.py
@@ -21,4 +21,4 @@ DELETE_OUTPUT_DIRECTORY = True
 # Following items are often useful when publishing
 
 DISQUS_SITENAME = 'fsantiago-info'
-#GOOGLE_ANALYTICS = ""
+GOOGLE_ANALYTICS = 'UA-150341719-1'
